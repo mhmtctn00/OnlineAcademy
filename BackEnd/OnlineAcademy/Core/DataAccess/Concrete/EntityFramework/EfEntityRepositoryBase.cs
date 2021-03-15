b884628974d2684523e791651d9219377ea6a362
mhmtctn00 <56972953+mhmtctn00@users.noreply.github.com>
2021-03-15 23:27:30 +0300

    Core Layer > Repository Pattern Added.

---
+++ BackEnd/OnlineAcademy/Core/DataAccess/Concrete/EntityFramework/EfEntityRepositoryBase.cs
@@ -0,0 +1,51 @@
+﻿using Core.DataAccess.Abstract;
+using Core.Entities.Abstract;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess.Concrete.EntityFramework
+{
+    public class EfEntityRepositoryBase<TEntity, TContext> : IEntityRepository<TEntity>
+        where TEntity : class, IEntity, new()
+        where TContext : DbContext, new()
+    {
+        private readonly DbContext _context;
+
+        public EfEntityRepositoryBase(DbContext context)
+        {
+            _context = context;
+        }
+        public async Task AddAsync(TEntity entity)
+        {
+            await _context.Set<TEntity>().AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(TEntity entity)
+        {
+            await Task.Run(() => { _context.Set<TEntity>().Remove(entity); });
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> predicate)
+        {
+            return await _context.Set<TEntity>().FirstOrDefaultAsync(predicate);
+        }
+
+        public async Task<IEnumerable<TEntity>> GetListAsync(Expression<Func<TEntity, bool>> predicate = null)
+        {
+            return await _context.Set<TEntity>().Where(predicate).ToListAsync()
+        }
+
+        public async Task UpdateAsync(TEntity entity)
+        {
+            await Task.Run(() => { _context.Set<TEntity>().Update(entity); });
+            await _context.SaveChangesAsync();
+        }
+    }
+}
