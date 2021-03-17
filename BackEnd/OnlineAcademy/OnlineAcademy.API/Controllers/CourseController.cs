efa625037a1de035f52c4cc77eb2218fc6f1769d
mhmtctn00 <56972953+mhmtctn00@users.noreply.github.com>
2021-03-17 17:38:14 +0300

    ..

---
+++ BackEnd/OnlineAcademy/OnlineAcademy.API/Controllers/CourseController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineAcademy.Business.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineAcademy.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CourseController : ControllerBase
+    {
+
+        private ICourseService _courseService;
+
+        public CourseController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var data = await _courseService.GetAllAsync();
+            return Ok(data);
+        }
+    }
+}
