14195d368ad9cbafc220ea75add24b44e9945849
mhmtctn00 <56972953+mhmtctn00@users.noreply.github.com>
2021-03-19 15:20:31 +0300

    Front End > Bootstrap and Angular Material added

--- FrontEnd/OnlineAcademy/src/app/app-routing.module.ts
+++ FrontEnd/OnlineAcademy/src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CourseComponent } from './views/course/course.component';
+import { CoursesComponent } from './views/courses/courses.component';
+import { HomeComponent } from './views/home/home.component';
+import { InstructorLayoutComponent } from './views/layouts/instructor-layout/instructor-layout.component';
+import { MainLayoutComponent } from './views/layouts/main-layout/main-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: MainLayoutComponent,
+    children: [
+      {
+        path: "",
+        component: HomeComponent
+      },
+      {
+        path: "course",
+        component: CourseComponent
+      },
+      {
+        path: "courses",
+        component: CoursesComponent
+      }
+    ]
+  },
+  {
+    path: "instructor",
+    component: InstructorLayoutComponent,
+    children: [
+
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- FrontEnd/OnlineAcademy/src/app/app.module.ts
+++ FrontEnd/OnlineAcademy/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainLayoutComponent } from './views/layouts/main-layout/main-layout.component';
+import { HomeComponent } from './views/home/home.component';
+import { CourseComponent } from './views/course/course.component';
+import { CoursesComponent } from './views/courses/courses.component';
+import { InstructorLayoutComponent } from './views/layouts/instructor-layout/instructor-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    HomeComponent,
+    CourseComponent,
+    CoursesComponent,
+    InstructorLayoutComponent
   ],
   imports: [
     BrowserModule,
